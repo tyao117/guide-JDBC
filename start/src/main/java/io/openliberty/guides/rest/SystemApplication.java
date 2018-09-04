fabce42c4982dea7179639804389c62e506557a9
Timothy Yao <Timothy.Yao@ibm.com>
2018-09-04 16:40:03 -0500

    took out files in the start side

--- start/src/main/java/io/openliberty/guides/rest/SystemApplication.java
+++
@@ -1,9 +0,0 @@
-package io.openliberty.guides.rest;
-
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("api")
-public class SystemApplication extends Application {
-
-}
